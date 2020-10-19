95faa3ff5248454c4a58a2b42a44b0372f844cef
Preston Hartzell <preston.hartzell@gmail.com>
2020-10-19 19:19:52 -0400

    script for directory of files

--- apply-incl.py
+++ apply-incl.py
@@ -1,4 +1,5 @@
 import json
+import glob
 import os
 import numpy as np
 import pdal
@@ -9,12 +10,10 @@ def get_socs(filename):
     # Convert LAS/LAZ point cloud from UTM to SOCS system:
     #   (1) UTM to ECEF, (2) Inverse POP, (3) Inverse SOP
     #   Note: SOP is hardcoded here for ATLAS_South-201908
-
     root, ext = os.path.splitext(filename)
-    outfilename = root + "-SOCS" + ext
-
+    outfilename = root + "-socs" + ext
     pdal_pipe = [
-        "{}".format(filename),
+        filename,
         {
             "type":"filters.reprojection",
             "in_srs":"EPSG:32624",
@@ -29,7 +28,8 @@ def get_socs(filename):
             "type":"filters.transformation",
             "matrix":"0.4560212389547335 -0.8899688924677321 -0.0000078707114301 830.9795317677513 0.8899669318671532 0.4560202157410177 0.0021030973542710 -3931.5277531064141 -0.0018681020196080 -0.0009660617340401 0.9999977884573395 512.8759432302256 0.0 0.0 0.0 1.0",
             "invert":"true"
-        }
+        },
+        outfilename
     ]
     p = pdal.Pipeline(json.dumps(pdal_pipe))
     p.validate()
@@ -37,10 +37,10 @@ def get_socs(filename):
     arrays = p.arrays
     view = arrays[0]
 
-    t = view('GpsTime')
+    t = view['GpsTime']
     x = view['X']
-    y = view['y']
-    z = view['z']
+    y = view['Y']
+    z = view['Z']
 
     return t, x, y, z
 
@@ -54,10 +54,11 @@ def get_incl(filename):
     t = incl[:,0]
     roll = incl[:,1]
     pitch = incl[:,2]
+
     return t, roll, pitch
 
 
-def filter_incl(incl, kernel_length=101)
+def filter_incl(incl, kernel_length=101):
     # Blackman window kernel
     kernel = np.blackman(kernel_length)
     kernel = kernel / np.sum(kernel)
@@ -71,19 +72,84 @@ def filter_incl(incl, kernel_length=101)
 
 
 def apply_incl(pt, x, y, z, it, roll, pitch):
-    
+    # Inclination time midpoints for masking
+    it_mid = (it[1:] + it[:-1]) / 2
+
+    # Rotate points according to closes inclination time
+    xyz_rot = np.zeros((len(x), 3))
+    for i in range(0, len(it)):
+        # Mask for points closest to current inclination time
+        if i == 0:
+            mask = pt <= it_mid[i]
+        elif i == (len(it) - 1):
+            mask = pt > it_mid[i-1]
+        else:
+            mask = np.logical_and(pt > it_mid[i-1], pt <= it_mid[i])
+        # Apply inclination rotations
+        R_roll = np.array([
+            [1, 0, 0],
+            [0, np.cos(np.deg2rad(roll[i])), -np.sin(np.deg2rad(roll[i]))],
+            [0, np.sin(np.deg2rad(roll[i])), np.cos(np.deg2rad(roll[i]))]
+        ])
+        R_pitch = np.array([
+            [np.cos(np.deg2rad(pitch[i])), 0, np.sin(np.deg2rad(pitch[i]))],
+            [0, 1, 0],
+            [-np.sin(np.deg2rad(pitch[i])), 0, np.cos(np.deg2rad(pitch[i]))],
+        ])
+        xyz_rot[mask,0:3] = (R_pitch @ R_roll @ np.vstack((x[mask], y[mask], z[mask]))).T
+
+    x_rot = xyz_rot[:,0]
+    y_rot = xyz_rot[:,1]
+    z_rot = xyz_rot[:,2]
+
+    return x_rot, y_rot, z_rot
 
 
+def save_socs(filename, t, x, y, z):
+    out_type = np.dtype([('GpsTime', t.dtype), ('X', x.dtype), ('Y', y.dtype), ('Z', z.dtype)])
+    out = np.empty(len(t), dtype=out_type)
+    out['GpsTime'] = t
+    out['X'] = x
+    out['Y'] = y
+    out['Z'] = z
+
+    # root, ext = os.path.splitext(filename)
+    # outfilename = root + "-SOCS" + ext
+
+    pdal_pipe = [
+        {
+            "type":"writers.las",
+            "filename":filename
+        }
+    ]
+
+    p = pdal.Pipeline(json=json.dumps(pdal_pipe), arrays=[out,])
+    p.validate()
+    p.execute()
 
 
-pnts_filename = "/mnt/e/ATLAS/south_200501-200515/laz/200501_072310.laz"
-incl_filename = "/mnt/e/ATLAS/south_200501-200515/rxp/mta/200501_072310-incl.txt"
+# Script
+laz_dir = "/mnt/e/ATLAS/south_200501-200515/laz"
+incl_dir = "/mnt/e/ATLAS/south_200501-200515/rxp/mta"
 
-pt, x, y, z = get_socs(pnts_filename)
-it, roll, pitch = get_incl(incl_filename)
+laz_files = [f for f in os.listdir(laz_dir) if f.endswith(".laz")]
 
-filt_roll = filtered_incl(roll, 101)
-filt_pitch = filtered_incl(pitch, 101)
+for laz_file in laz_files:
+    laz_file = laz_dir + "/" + laz_file
+    print("Applying inclination to {}".format(laz_file))
 
+    root, ext = os.path.splitext(os.path.basename(laz_file))
+    incl_file = incl_dir + "/" + root + "-incl.txt"
 
+    pt, x, y, z = get_socs(laz_file)
+    it, roll, pitch = get_incl(incl_file)
+
+    filtered_roll = filter_incl(roll, 101)
+    filtered_pitch = filter_incl(pitch, 101)
+
+    xr, yr, zr = apply_incl(pt, x, y, z, it, filtered_roll, filtered_pitch)
+
+    root, ext = os.path.splitext(laz_file)
+    outfilename = root + "-socs-incl_applied" + ext
+    save_socs(outfilename, pt, xr, yr, zr)
 
